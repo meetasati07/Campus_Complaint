87f35d6483fd5a7274e317540e7c843aff7a2429
meetasati07 <mmasati1976@gmail.com>
2026-03-09 18:54:48 +0530

    Separated Theme Data into diff file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:campus_complaint/pages/landing_page.dart';
+import 'package:campus_complaint/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,58 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xffbadfdb),
-          toolbarHeight: 80,
-          titleTextStyle: TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Poppins',
-            letterSpacing: 1.5,
-          ),
-          centerTitle: true,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.circular(18),
-          ),
-        ),
-        scaffoldBackgroundColor: Color.fromARGB(255, 255, 119, 119),
-        fontFamily: 'Roboto',
-        primaryColor: Color(0xfffff2eb),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            backgroundColor: Colors.black,
-            foregroundColor: Colors.white,
-            padding: EdgeInsets.symmetric(vertical: 11, horizontal: 20),
-            textStyle: TextStyle(
-              fontFamily: 'Roboto',
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.2
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadiusGeometry.circular(10),
-              side: BorderSide(
-                width: 3,
-                color: Colors.white
-              ),
-            ),
-          ),
-        ),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: Color(0xffbadfdb),
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.black
-        ),
-        cardTheme: CardThemeData(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.circular(14)
-          ),
-          elevation: 5
-        )
-      ),
+      theme: AppTheme.customThemeData(),
       home: LandingPage(),
     );
   }
---
+++ lib/theme.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData customThemeData() => ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: Color(0xffbadfdb),
+      toolbarHeight: 80,
+      titleTextStyle: TextStyle(
+        color: Color.fromARGB(255, 255, 255, 255),
+        fontSize: 28,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins',
+        letterSpacing: 1.5,
+      ),
+      centerTitle: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(18),
+      ),
+    ),
+    scaffoldBackgroundColor: Color.fromARGB(255, 255, 119, 119),
+    fontFamily: 'Roboto',
+    primaryColor: Color(0xfffff2eb),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        padding: EdgeInsets.symmetric(vertical: 11, horizontal: 20),
+        textStyle: TextStyle(
+          fontFamily: 'Roboto',
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.2,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(10),
+          side: BorderSide(width: 3, color: Colors.white),
+        ),
+      ),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: Color(0xffbadfdb),
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.black,
+    ),
+    cardTheme: CardThemeData(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(14),
+      ),
+      elevation: 5,
+    ),
+  );
+}
