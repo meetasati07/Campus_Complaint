a6165dab23e35b39a3e15c0c94e15ff7011ada2c
meetasati07 <mmasati1976@gmail.com>
2026-03-09 17:52:49 +0530

    Defined Theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:campus_complaint/pages/landing_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,24 +11,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Campus Complaint'), centerTitle: true),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Welcome to Campus Complaint',
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'Raise your issue faced in the campus',
-                style: TextStyle(fontSize: 18),
-              ),
-            ],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xffbadfdb),
+          toolbarHeight: 80,
+          titleTextStyle: TextStyle(
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Poppins',
+            letterSpacing: 1.5,
+          ),
+          centerTitle: true,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(18),
           ),
         ),
+        scaffoldBackgroundColor: Color.fromARGB(255, 255, 119, 119),
+        fontFamily: 'Roboto',
+        primaryColor: Color(0xfffff2eb),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            padding: EdgeInsets.symmetric(vertical: 11, horizontal: 20),
+            textStyle: TextStyle(
+              fontFamily: 'Roboto',
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.2
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(10),
+              side: BorderSide(
+                width: 3,
+                color: Colors.white
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Color(0xffbadfdb),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.black
+        ),
+        cardTheme: CardThemeData(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(14)
+          ),
+          elevation: 5
+        )
       ),
+      home: LandingPage(),
     );
   }
 }
---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: AppBar(title: Text('Campus Complaint')),
+      body: Column(),
+    );
+  }
+}
