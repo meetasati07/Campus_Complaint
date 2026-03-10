d15302c9b6d62949d939a760ebc4d91fb6608333
meetasati07 <mmasati1976@gmail.com>
2026-03-10 19:26:47 +0530

    Fixed the screen overflow issue

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -25,97 +25,98 @@ class SignupPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Sign Up')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadiusGeometry.circular(75),
-              child: Image(
-                image: AssetImage('assets/profile.jpg'),
-                height: 150,
-                width: 150,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Name',
-                      hintStyle: hintStyle,
-                      fillColor: Colors.blue,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(24),
-                        borderSide: BorderSide.none,
-                      ),
-                      filled: true,
-                    ),
-                    style: inputTextStyle,
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      hintStyle: hintStyle,
-                      fillColor: Colors.blue,
-                      border: border,
-                      filled: true,
-                    ),
-                    style: inputTextStyle,
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Phone Number',
-                      hintStyle: hintStyle,
-                      fillColor: Colors.blue,
-                      border: border,
-                      filled: true,
-                    ),
-                    style: inputTextStyle,
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: hintStyle,
-                      fillColor: Colors.blue,
-                      border: border,
-                      filled: true,
-                    ),
-                    style: inputTextStyle,
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'College Name',
-                      hintStyle: hintStyle,
-                      fillColor: Colors.blue,
-                      border: border,
-                      filled: true,
-                    ),
-                    style: inputTextStyle,
-                  ),
-                ],
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (_) => BottomNavigation()),
-                  ModalRoute.withName('/Home'),
-                );
-              },
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-              ),
-              child: Text('Sign Up'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                ClipRRect(
+                  borderRadius: BorderRadiusGeometry.circular(75),
+                  child: Image(
+                    image: AssetImage('assets/profile.jpg'),
+                    height: 150,
+                    width: 150,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Name',
+                          hintStyle: hintStyle,
+                          fillColor: Colors.blue,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                        ),
+                        style: inputTextStyle,
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          hintStyle: hintStyle,
+                          fillColor: Colors.blue,
+                          border: border,
+                          filled: true,
+                        ),
+                        style: inputTextStyle,
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Phone Number',
+                          hintStyle: hintStyle,
+                          fillColor: Colors.blue,
+                          border: border,
+                          filled: true,
+                        ),
+                        style: inputTextStyle,
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: hintStyle,
+                          fillColor: Colors.blue,
+                          border: border,
+                          filled: true,
+                        ),
+                        style: inputTextStyle,
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'College Name',
+                          hintStyle: hintStyle,
+                          fillColor: Colors.blue,
+                          border: border,
+                          filled: true,
+                        ),
+                        style: inputTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (_) => BottomNavigation()),
+                      ModalRoute.withName('/Home'),
+                    );
+                  },
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                  ),
+                  child: Text('Sign Up'),
+                ),
+                SizedBox(height: 10),
                 Text(
                   'Already have an account? ',
                   style: TextStyle(
@@ -135,7 +136,7 @@ class SignupPage extends StatelessWidget {
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
