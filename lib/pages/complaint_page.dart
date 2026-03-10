1cd3fe3332bf11a5c413743a452d00d77710b66e
meetasati07 <mmasati1976@gmail.com>
2026-03-10 21:56:50 +0530

    feat: Complaint Form Validation

--- lib/pages/complaint_page.dart
+++ lib/pages/complaint_page.dart
@@ -1,3 +1,4 @@
+import 'package:campus_complaint/widgets/complaint_form.dart';
 import 'package:flutter/material.dart';
 
 class ComplaintPage extends StatelessWidget {
@@ -5,89 +6,14 @@ class ComplaintPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TextStyle hintStyle = TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 24,
-      fontWeight: FontWeight.bold,
-    );
-    final OutlineInputBorder border = OutlineInputBorder(
-      borderRadius: BorderRadius.circular(24),
-      borderSide: BorderSide.none,
-    );
-    final TextStyle inputTextStyle = TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 24,
-      fontWeight: FontWeight.bold,
-      color: Colors.white,
-    );
     return Scaffold(
       appBar: AppBar(title: Text('Complaint')),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Title',
-                    hintStyle: hintStyle,
-                    fillColor: Colors.blue,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(24),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                  ),
-                  style: inputTextStyle,
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Description',
-                    hintStyle: hintStyle,
-                    fillColor: Colors.blue,
-                    border: border,
-                    filled: true,
-                  ),
-                  style: inputTextStyle,
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Image',
-                    hintStyle: hintStyle,
-                    fillColor: Colors.blue,
-                    border: border,
-                    filled: true,
-                  ),
-                  style: inputTextStyle,
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Issue Location',
-                    hintStyle: hintStyle,
-                    fillColor: Colors.blue,
-                    border: border,
-                    filled: true,
-                  ),
-                  style: inputTextStyle,
-                ),
-              ],
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-            ),
-            child: Text('Raise \nComplaint', textAlign: TextAlign.center),
-          ),
+          Padding(padding: const EdgeInsets.all(24.0), child: ComplaintForm()),
         ],
       ),
-      
     );
   }
 }
---
+++ lib/widgets/complaint_form.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class ComplaintForm extends StatefulWidget {
+  const ComplaintForm({super.key});
+
+  @override
+  State<ComplaintForm> createState() => _ComplaintFormState();
+}
+
+class _ComplaintFormState extends State<ComplaintForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextStyle hintStyle = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+  final OutlineInputBorder border = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(24),
+    borderSide: BorderSide.none,
+  );
+  final TextStyle inputTextStyle = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Title',
+              hintStyle: hintStyle,
+              fillColor: Colors.blue,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(24),
+                borderSide: BorderSide.none,
+              ),
+              filled: true,
+            ),
+            style: inputTextStyle,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Title Required';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Description',
+              hintStyle: hintStyle,
+              fillColor: Colors.blue,
+              border: border,
+              filled: true,
+            ),
+            style: inputTextStyle,
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Issue Location',
+              hintStyle: hintStyle,
+              fillColor: Colors.blue,
+              border: border,
+              filled: true,
+            ),
+            style: inputTextStyle,
+          ),
+          SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+            ),
+            child: Text('Raise \nComplaint', textAlign: TextAlign.center),
+          ),
+        ],
+      ),
+    );
+  }
+}
